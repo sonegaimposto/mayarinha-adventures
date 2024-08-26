9ab3d23de36661ef2822a726fb66de1df7e06ae2
raffico <raffael.vieira@escola.pr.gov.br>
2024-08-26 10:29:18 -0300

    .

--- main.js
+++ main.js
@@ -3,6 +3,11 @@ const pipe = document.querySelector(".pipe");
 
 const jump = () => {
     mayara.classList.add("jump");
+
+    setTimeout(() => {
+        mayara.classList.remove("jump");
+    }
+    ,500);
 }
 document.addEventListener("keydown", jump);
 
