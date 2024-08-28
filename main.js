6fbd4790da7f05cd9578686be916d2fba560e765
raffico <raffael.vieira@escola.pr.gov.br>
2024-08-28 11:26:50 -0300

    .

--- main.js
+++ main.js
@@ -18,8 +18,14 @@ const loop = setInterval (() => {
         pipe.style.animation = "none";
         pipe.style.left = `${pipePosition}px`;
 
-        pipe.style.animation = "none";
-        pipe.style.left = `${pipePosition}px`;
+        mario.style.animation = "none";
+        mario.style.left = `${marioPosition}px`;
+
+        mario.src = "./img/game-over.png";
+        mario.style.width = "75px";
+        mario.style.marginLeft = "50px";
+
+        clearInterval(loop);
     }
 });
 
